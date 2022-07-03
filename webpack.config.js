024c2e671b6197ad3dcf4b7d1ba33654ab75021c
MoonCheung <ikmoons.is@gmail.com>
2022-07-03 21:52:33 +0800

    chore: 移动新目录

--- webpack.config.js
+++ webpack.config.js
@@ -17,36 +17,33 @@ module.exports = {
       // 解析 CSS
       {
         test: /\.css$/i,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-        ]
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
       },
       // 解析 Less
       {
         test: /\.less$/i,
         use: [
-          { loader: "style-loader" },
+          { loader: 'style-loader' },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               modules: {
                 mode: (resourcePath) => {
                   if (/pure.css$/i.test(resourcePath)) {
-                    return "pure";
+                    return 'pure';
                   }
                   if (/global.css$/i.test(resourcePath)) {
-                    return "global";
+                    return 'global';
                   }
-                  return "local";
-                },
+                  return 'local';
+                }
               }
             }
           },
-          { loader: "less-loader" },
-        ],
-      },
-    ],
+          { loader: 'less-loader' }
+        ]
+      }
+    ]
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.less', 'css'],
@@ -56,7 +53,7 @@ module.exports = {
       components: path.join(__dirname, 'src/components/'),
       apis: path.join(__dirname, 'src/apis/'),
       hooks: path.join(__dirname, 'src/hooks/'),
-      store: path.join(__dirname, 'src/store/'),
+      store: path.join(__dirname, 'src/store/')
     }
   },
   devtool: 'inline-source-map',
@@ -64,18 +61,18 @@ module.exports = {
   devServer: {
     static: './dist',
     port: 3000,
-    hot: true,
+    hot: true
   },
   // 默认输出
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    clean: true
   },
   // 指定模板 html
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
+      template: './public/index.html'
+    })
+  ]
 };
